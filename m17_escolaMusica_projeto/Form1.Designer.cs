6642fa0d404f23ab7e8eea558d5d397c2ac388fa
leonardodsferreira <leonardocontasup@gmail.com>
2025-11-24 11:03:47 +0000

    setup

---
+++ m17_escolaMusica_projeto/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace m17_escolaMusica_projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_nome = new System.Windows.Forms.Label();
+            this.lb_email = new System.Windows.Forms.Label();
+            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.lb_idade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_nome
+            // 
+            this.lb_nome.AutoSize = true;
+            this.lb_nome.Location = new System.Drawing.Point(159, 98);
+            this.lb_nome.Name = "lb_nome";
+            this.lb_nome.Size = new System.Drawing.Size(38, 13);
+            this.lb_nome.TabIndex = 0;
+            this.lb_nome.Text = "Nome:";
+            // 
+            // lb_email
+            // 
+            this.lb_email.AutoSize = true;
+            this.lb_email.Location = new System.Drawing.Point(162, 156);
+            this.lb_email.Name = "lb_email";
+            this.lb_email.Size = new System.Drawing.Size(35, 13);
+            this.lb_email.TabIndex = 1;
+            this.lb_email.Text = "Email:";
+            // 
+            // tb_nome
+            // 
+            this.tb_nome.Location = new System.Drawing.Point(267, 98);
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.Size = new System.Drawing.Size(100, 20);
+            this.tb_nome.TabIndex = 2;
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(267, 156);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(100, 20);
+            this.tb_email.TabIndex = 3;
+            // 
+            // lb_idade
+            // 
+            this.lb_idade.AutoSize = true;
+            this.lb_idade.Location = new System.Drawing.Point(162, 213);
+            this.lb_idade.Name = "lb_idade";
+            this.lb_idade.Size = new System.Drawing.Size(36, 13);
+            this.lb_idade.TabIndex = 4;
+            this.lb_idade.Text = "idade:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb_idade);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.lb_email);
+            this.Controls.Add(this.lb_nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_nome;
+        private System.Windows.Forms.Label lb_email;
+        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.Label lb_idade;
+    }
+}
+
